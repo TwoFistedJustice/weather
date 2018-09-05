7be794fab18a9abd5a45c90a220c64d5936b88f9
TwoFistedJustice <phaltak-1@yahoo.com>
2018-09-05 15:42:01 -0700

    Implement a central data object
    Add a central object to hold relevant data and the functions to set the
    values in the object.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const darkSkyKey = config.darkSkyKey;
 const argv = yargs
   .options({
     a: {
-      describe: 'Get weather for a given location.',
+      describe: 'Get weather for a given location. Use double quotes in Windows.',
       demand: true,
       alias: 'address',
       string: true
@@ -28,18 +28,17 @@ var fetchedData = {
     lng: -117.8287845,
     formatted: null
   },
-  placeName: null,
   current: {
     temperature: null,
     apparentTemperature: null,
     uvIndex: null
   },
   daily: {
-    summary: null,
+    summaryWeekly: null,
     temperatureHigh: null,
     apparentTemperatureHigh:null,
     ozone: null,
-    uvindex: null
+    uvIndex: null
   },
   time: {
     apparentTemperatureHighTime:null,
@@ -51,21 +50,27 @@ axios.get(geoCodeURL)
    if (response.data.status === 'ZERO_RESULTS') {
      throw new Error('Gack! Address not found! What do I do? What do I DOOOO???');
    }
-
-   fetchedData.location.lat = response.data.results[0].geometry.location.lat;
-   fetchedData.location.lng = response.data.results[0].geometry.location.lng;
-   fetchedData.location.formatted = response.data.results[0].formatted_address;
    
-   var weather = `https://api.darksky.net/forecast/${darkSkyKey}/${fetchedData.location.lat},${fetchedData.location.lng}`;
-   fetchedData.location.name = response.data.results[0].address_components[1].short_name;
+   recordLocationData(response.data.results[0]);
    
-   return axios.get(weather);
+   var weatherData = `https://api.darksky.net/forecast/${darkSkyKey}/${fetchedData.location.lat},${fetchedData.location.lng}`;
+   return axios.get(weatherData);
  })
   .then((response)=>{
-    let temp = Math.round(response.data.currently.temperature);
-    let apparentTemp = Math.round(response.data.currently.apparentTemperature);
-    console.log(`The temperature in ${fetchedData.location.name} is ${temp} but it feels like ${apparentTemp}`);
+  
+    recordCurrentData(response.data.currently);
+    recordDailyData(response.data.daily);
+    // fetchedData.current.temperature = Math.round(response.data.currently.temperature);
+    // fetchedData.current.apparentTemperature = Math.round(response.data.currently.apparentTemperature);
+    // fetchedData.current.apparentTemperatureHigh = Math.round(response.data.daily.apparentTemperatureHigh);
+    // fetchedData.current.ozone = Math.round(response.data.daily.ozone);
+    // fetchedData.current.uvIndex = Math.round(response.data.daily.uvIndex);
     
+    // console.log(JSON.stringify(response.data.daily.data[0], undefined, 2));
+    console.log(JSON.stringify(fetchedData, undefined, 2));
+    
+    
+    console.log(`The temperature in ${fetchedData.location.name} is ${fetchedData.current.temperature} but it feels like ${fetchedData.current.apparentTemperature}`);
   })
   .catch((err)=>{
    if(err.code === 'ENOTFOUND') {
@@ -73,5 +78,33 @@ axios.get(geoCodeURL)
    } else {
      console.log('thrown from .catch block',err.message);
    }
-   
 });
+
+var recordLocationData = (geoResults) => {
+  //geoReults is response.data.results[0]
+  fetchedData.location.name = geoResults.address_components[1].short_name;
+  fetchedData.location.lat = geoResults.geometry.location.lat;
+  fetchedData.location.lng = geoResults.geometry.location.lng;
+  fetchedData.location.formatted = geoResults.formatted_address;
+};
+
+
+var recordCurrentData = (currently) => {
+  fetchedData.current.temperature = Math.round(currently.temperature);
+  fetchedData.current.apparentTemperature = Math.round(currently.apparentTemperature);
+  fetchedData.current.uvIndex = Math.round(currently.uvIndex);
+  
+};
+
+
+var recordDailyData = (daily) => {
+  console.log(JSON.stringify(daily.data[0], undefined, 2));
+  fetchedData.daily.summaryWeekly = daily.summary;
+  fetchedData.daily.temperatureHigh = Math.round(daily.data[0].temperatureHigh);
+  fetchedData.daily.apparentTemperatureHigh = Math.round(daily.data[0].apparentTemperatureHigh);
+  fetchedData.daily.ozone = Math.round(daily.data[0].ozone);
+  fetchedData.daily.uvIndex = Math.round(daily.data[0].uvIndex);
+  fetchedData.time.apparentTemperatureHighTime = daily.data[0].apparentTemperatureHighTime;
+  fetchedData.time.uvIndexTime = daily.data[0].uvIndexTime;
+  
+};
